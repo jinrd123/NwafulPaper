7c39863f16a7a9ac3e47c8a44423b723f0b16314
靳荣达 <2368741842@qq.com>
2022-12-09 20:34:32 +0800

    解决background页面月亮光晕被遮挡问题

--- src/pages/BackGround/index.vue
+++ src/pages/BackGround/index.vue
@@ -165,6 +165,8 @@ section img {
 section img#moon {
   /* mix-blend-mode CSS 属性描述了元素的内容应该与元素的直系父元素的内容和元素的背景如何混合。 */
   mix-blend-mode: screen;
+  width: 86%;
+  top: 50px;
 }
 
 section img#mountains_front {
